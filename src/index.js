16136ac43293e845ba409dd178a3c684fb6e164b
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2023-09-12 00:36:59 +0300

    added homework 11

---
+++ src/index.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+
+
+import Notiflix from 'notiflix';
+
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+
+
+const searchForm = document.querySelector('#search-form');
+
+const input = document.querySelector('#searchQuery');
+
+const gallery = document.querySelector('.gallery');
+
+const button = document.querySelector('.load-more');
+
+console.log(button);
+
+
+console.log(searchForm);
+
+let page = 1;
+
+const serverRequest = async () => {
+
+    page = 1;
+
+    if (!button.classList.contains("phantom")) {
+        button.classList.add(phantom);
+
+    }
+
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?key=39382301-87481c6222a57772410795ead&q=${input.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+        
+        if (!response.data.hits.length > 0) {
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            return
+        }
+        button.classList.remove("phantom");
+        return gallery.innerHTML = listCreate(response.data.hits);
+
+    } catch (error) {
+        Notiflix.Notify.failure("ERROR!");
+    }
+}
+
+const listCreate = function (hits) {
+    const addList = hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" data-img ="${largeImageURL}"/>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`).join('');
+    
+    return addList;
+}
+
+const loadMore = async function () {
+    page += 1;
+    
+    
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?key=39382301-87481c6222a57772410795ead&q=${input.value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+        gallery.insertAdjacentHTML('beforeend', listCreate(response.data.hits));
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+searchForm.addEventListener("submit", (event) => { event.preventDefault(); serverRequest() });
+button.addEventListener('click', loadMore)
